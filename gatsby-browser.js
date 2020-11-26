07e636a6d9d47637cdb6773ff0f1a3dada8280ea
zh <50803056+hhzule@users.noreply.github.com>
2020-11-26 14:36:56 +0500

    Add files via upload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,16 @@
 import './src/styles/global.css'
+import firebase from "gatsby-plugin-firebase";
+import "firebase/auth"
+import "firebase/app"
+import "firebase/functions"
+
+import React from 'react'
+import { FirebaseProvider } from "./src/service";
+
+
+
+export const wrapRootElement = ({ element }) => {
+    return (
+        <FirebaseProvider >{element}</FirebaseProvider>
+    );
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 const dotenv = require("dotenv");
 
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config();
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     dotenv.config();
+// }
 
+dotenv.config();
 module.exports = {
     plugins: [
         "gatsby-plugin-typescript",
@@ -14,5 +15,19 @@ module.exports = {
                 accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
             },
         },
+        {
+            resolve: "gatsby-plugin-firebase",
+            options: {
+                credentials: {
+                    apiKey: "AIzaSyAzfLQuBLoj5EpojvqqZi-2wnLKtCMNi7c",
+                    authDomain: "gatsbyblo.firebaseapp.com",
+                    databaseURL: "https://gatsbyblo.firebaseio.com",
+                    projectId: "gatsbyblo",
+                    storageBucket: "gatsbyblo.appspot.com",
+                    messagingSenderId: "383675647403",
+                    appId: "1:383675647403:web:5129ead220562fbe81beee",
+                }
+            }
+        }
     ],
 };
